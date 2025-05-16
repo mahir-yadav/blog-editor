539b86e9a6c0204081f9c25ad87ba30340f77a00
mahir <mahir21263@iiitd.ac.in>
2025-05-16 22:08:42 +0530

    shifted db from local host to atlas

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv')
+dotenv.config();
 const blogRoutes = require('./routes/blogroutes');
 
 const app = express();
@@ -16,7 +18,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogRoutes);
 
-mongoose.connect('mongodb://localhost:27017/blogEditorDB')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
